089a282c70f800283bc45f59477e115cfaf5a6fd
ppgroup2020 <ppperformance.group@gmail.com>
2025-09-06 17:34:02 +0100

    Delete nn directory

--- nn/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const app = express();
-app.use(cors());
-app.get('/api/health', (req,res)=> res.json({ok:true,name:'PP Group API'}));
-app.use('/', express.static(path.join(__dirname,'..','client')));
-app.listen(process.env.PORT||3000, ()=> console.log('PP Group API a correr na porta', process.env.PORT||3000));
